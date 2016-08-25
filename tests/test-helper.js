f02584fb01a43e3b3a16a2e95ffa70b39f433fc1
Robert Jackson <robert.w.jackson@me.com>
2016-08-25 10:57:00 -0400

    Guard `Ember.Debug.registerDeprecationHandler` usage for Ember < 2.1.0.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,11 +6,14 @@ import {
 
 setResolver(resolver);
 
-Ember.Debug.registerDeprecationHandler((message, options, next) => {
-  if (message.indexOf('includes') !== -1 || message.indexOf('contains')) {
-    throw new Error(`Deprecation message for includes/contains: ${message}`);
-  } else {
-    // defer to whatever handler was registered before this one
-    next(message, options);
-  }
-});
+// support testing against Ember < 2.1
+if (Ember.Debug && Ember.Debug.registerDeprecationHandler) {
+  Ember.Debug.registerDeprecationHandler((message, options, next) => {
+    if (message.indexOf('includes') !== -1 || message.indexOf('contains')) {
+      throw new Error(`Deprecation message for includes/contains: ${message}`);
+    } else {
+      // defer to whatever handler was registered before this one
+      next(message, options);
+    }
+  });
+}
