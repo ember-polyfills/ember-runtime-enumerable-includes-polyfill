0c55bd798abb580277d6128664376cf55948beb2
Robert Jackson <robert.w.jackson@me.com>
2016-08-25 10:11:02 -0400

    Initial implementation.

--- index.js
+++ index.js
@@ -2,5 +2,40 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-runtime-enumerable-includes-polyfill'
+  name: 'ember-runtime-enumerable-includes-polyfill',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    var importContext;
+
+    if (this.import) {  // support for ember-cli >= 2.7
+      importContext = this;
+    } else { // addon support for ember-cli < 2.7
+      importContext = this._findHostForLegacyEmberCLI();
+    }
+
+    var VersionChecker = require('ember-cli-version-checker');
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember', 'bower');
+
+    if (dep.lt('2.8.0-beta.1')) {
+      importContext.import('vendor/enumerable-includes-polyfill/index.js');
+    }
+  },
+
+  // included from https://git.io/v6F7n
+  // not needed for ember-cli > 2.7
+  _findHostForLegacyEmberCLI: function() {
+    var current = this;
+    var app;
+
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+
+    return app;
+  }
 };
